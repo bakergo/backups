36bac178ac69420851a6bf443947887b4ae1ee71
Greg Baker <bakergo@gmail.com>
2014-12-20 22:22:26 -0800

    Backup script is updated a bit

---
+++ backup-hourly.py
@@ -0,0 +1,157 @@
+#!/usr/bin/python
+'''
+Starts an incremental backup using duplicity, with periodic backups over time.
+This is meant to be a personal script and so the coding standards might be a
+little bit lacking, since there's nobody to code review.
+'''
+# The basic process for this backup is this:
+# - Do a zfs snapshot. This will ensure that the FS does not change throughout
+#   the backup process
+# - Run a duplicity snapshot on the backup. This should be full if beyond a
+#   certain timeframe (2 weeks?). That way if something gets hosed, the biggest
+#   loss is over 2 weeks.
+# - Optionally, run a verification on the old backups
+# - Delete the old backups if older than 2 backup cycles. This is to save $$.
+#   Think about this before implementing (If it runs away it could blow up
+#   everything).
+# - On each of these, have a sensible strategy on failure.
+# - Mail me on failure of the job
+# - Mail me on success of the job. Really I've got like 1TB of gmail space
+#   these 1k mails do nothing.
+# - Delete the zfs snapshot. This should not happen if any steps fail.
+
+import argparse
+import datetime
+import subprocess
+# import boto
+import logging
+import os
+import sys
+
+CONFIG = {'s3url': None, 'root': None, 'filesystem': None}
+CONFIG_FILE = '/etc/local/backups.conf'
+FORMAT = '%(asctime)-15s : %(levelname)-10s - %(msg)'
+DEBUG = False
+logging.basicConfig(format=FORMAT)
+LOG = logging.getLogger('root')
+
+def _Exec(*args):
+    '''
+    Executes the given command, and pipes all of the output to the standard
+    logger
+    '''
+    print ' '.join(args)
+    proc = subprocess.Popen(args, stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT)
+    line = proc.stdout.readline()
+    while line:
+        print line,
+        line = proc.stdout.readline()
+    if proc.wait() != 0:
+        raise IOError("Process crashed. Might want to take a look at that.")
+
+def timestamp():
+    ''' Returns teht imesamp in seconds '''
+    return int((datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)).total_seconds())
+
+class Duplicity(object):
+    def __init__(self,  url, dryrun, path, ekey_id, skey_id):
+        self.url = url
+        self.dryrun = dryrun
+        self.path = path
+        self.enc_key = ekey_id
+        self.sign_key = skey_id
+
+    def Verify(self):
+        ''' calls duplciity verify on the thing '''
+        pass
+
+    def Backup(self):
+        '''Calls duplicity backup'''
+        cmd = ['/usr/local/bin/duplicity',
+                '--use-agent',
+                '--full-if-older-than' , TimeFormat(weeks=2),
+                '--exclude', '**/nobackups',
+                '--exclude-if-present', '.nobackups',
+                '--encrypt-key', self.enc_key,
+                '--sign-key', self.sign_key,
+                '--allow-source-mismatch']
+        if self.dryrun:
+            cmd += ['--dry-run']
+        cmd += [self.path, self.url]
+        _Exec(*cmd)
+
+class Snapshot(object):
+    def __init__(self, filesystem):
+        super(Snapshot, self).__init__()
+        self.filesystem = filesystem
+        self.timestamp = timestamp()
+        # self.name = '%s@%d' % (self.filesystem, self.timestamp)
+        self.name = '%s@%s' % (self.filesystem, 'duplicity')
+
+    def Exists(self):
+        ''' Returns true if the snapshot path exists '''
+        return os.path.exists(self.name)
+
+    def __enter__(self):
+        ''' Takes a temporary ZFS snapshot, s I don't have to worry about it.'''
+        _Exec('zfs', 'snapshot', self.name)
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        ''' Removes the ZFS snapshot, so that I don't have to worry about it later. '''
+        _Exec('zfs', 'destroy', self.name)
+
+    def rebase(self, root, path):
+        return os.path.join(root, '.zfs', 'snapshot',
+                'duplicity', path)
+                # str(self.timestamp), path)
+
+def TimeFormat(years=0, months=0, weeks=0, days=0, hours=0, minutes=0, seconds=0):
+    '''Returns a relative time format as specified in the duplicity'''
+    symbols = ((years, 'Y'), (months, 'M'), (weeks, 'W'), (days, 'D'),
+            (hours, 'h'), (minutes, 'm'), (seconds, 's'))
+    fmt = []
+    for symbol in symbols:
+        if symbol[0] > 0:
+            fmt.extend((str(x) for x in symbol))
+    return ''.join(fmt)
+
+def ParseArguments(config):
+    parser = argparse.ArgumentParser(description='Run by backups, hourly')
+    # print config
+    parser.add_argument('--filesystem', type=str,
+            default=config['filesystem'],
+            help="ZFS filesystem to take a snapshot of")
+    parser.add_argument('--root', type=str, help="Path to back up", default=config['root'])
+    parser.add_argument('--s3url', type=str, help="S3 URL to upload to", default=config['s3url'])
+    parser.add_argument('-c', '--config', type=str, help="Configuration file")
+    parser.add_argument('-d', '--dryrun', type=str, help="Dry run")
+    parser.add_argument('-e', '--encryption_key_id', type=str, help="Do stuff", default=config['encrypt_key_id'])
+    parser.add_argument('-s', '--signing_key_id', type=str, help="Do stuff", default=config['sign_key_id'])
+    return parser.parse_args()
+
+def Backup(opts, config):
+    snap = Snapshot(opts.filesystem)
+    if snap.Exists():
+        print "Snapshot already exists, exiting early"
+        return
+    with snap:
+        duplicity = Duplicity(config['s3url'], DEBUG,
+                snap.rebase(opts.root, ''),
+                opts.encryption_key_id,
+                opts.signing_key_id)
+        duplicity.Backup()
+
+def main():
+    if os.geteuid() != 0:
+        sys.stderr.write("Requires a uid of r00t\n")
+        sys.exit(1)
+    config = {}
+    execfile(CONFIG_FILE, config)
+    args = ParseArguments(config)
+    Backup(args, config)
+
+if __name__ == '__main__':
+    main()
+
