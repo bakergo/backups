38a2fd2fae0b14aff19b8f66a12ee7ad4acb5114
Greg Baker <bakergo@gmail.com>
2023-11-29 20:40:45 -0800

    Take some better patterns from backup-mpu here

--- backup-home.sh
+++ backup-home.sh
@@ -1,5 +1,33 @@
-#!/bin/sh
-SRC=/home/
-DEST=/storage/shared/backup/jet/home/
-rsync -x -rlogtD --delete-after -q "${SRC}" "${DEST}"
+#!/bin/bash
+declare -a excludes=(
+  */.cache/go-build
+  */.cache/gopls
+  */.cache/mozilla
+  */.cache/duplicity
+  */.config/discord
+  */.local/share/Steam
+  */.mozilla
+  */Downloads/
+  */go/
+  */src/github.com/grpc
+  */src/github.com/protobuf
+  */src/go/
+  */src/zfs/
+  */src/wallabag/
+  */src/golang/
+  opt/dolphin-emu
+  */ttl
+)
+
+declare -a exclude_fmt=()
+for e in "${excludes[@]}" ; do
+  exclude_fmt+=("--exclude=/$e")
+done
+
+BACKUP_ROOT=/home
+BACKUP_DEST=/storage/shared/backup/jet/home
+rsync -rlogtD --delete-after -q \
+  "${exclude_fmt[@]}" \
+  --filter="dir-merge,n- .nobackups" \
+  "${BACKUP_ROOT}/" "${BACKUP_DEST}/"
 
